ff40a0fa85186b21e3c2a3f62e2ca7c3888cb7fb
Smith Krengkrud <smith.kre@gmail.com>
2019-11-10 13:43:51 +0700

    fix stacked button bug

--- iOSPhotoEditor/PhotoEditor+Gestures.swift
+++ iOSPhotoEditor/PhotoEditor+Gestures.swift
@@ -200,7 +200,9 @@ extension PhotoEditorViewController : UIGestureRecognizerDelegate  {
         if recognizer.state == .ended {
             imageViewToPan = nil
             lastPanPoint = nil
-            hideToolbar(hide: false)
+            if !isTyping {
+                hideToolbar(hide: false)
+            }
             deleteView.isHidden = true
             let point = recognizer.location(in: self.view)
             
